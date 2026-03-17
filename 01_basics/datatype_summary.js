e1b544c56a1e5ef8543e837ee9dc5145b67dcf7e
PingPong06 <darshanwaghwce@gmail.com>
2026-03-17 05:45:10 +0000

    learned classification of datatypes

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,11 @@
+// 2 Types: Primitive, Non-Primitive
+
+// Primitive: 7 Types
+// String, number, boolean, null, undefined, symbol, BigInt
+
+
+// Symbol: It is used to identify each data uniquely
+ console.log(Symbol("1") === Symbol("1")); // false
+
+//Non-Primitive (Reference): Array, object, Functions
+// Theorectically every non-primitive is of object datatype
