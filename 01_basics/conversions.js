9921ebb4d1974712999b243d7b57e6193c1e9dc6
PingPong06 <darshanwaghwce@gmail.com>
2026-03-17 05:10:16 +0000

    did some play with operators

--- 01_basics/conversions.js
+++ 01_basics/conversions.js
@@ -18,15 +18,31 @@ let boolNull = Boolean(nullValue);
 
 //let's check their values first
 
-console.log(newName);  // Darshan(string) = NaN(Number)
-console.log(newEmpty);  // ""(string) = 0(Number)
-console.log(newNullValue); // null(object) = 0(Number)
-console.log(newIsBoolean); // false(boolean) = false(string)
-console.log(boolNull);  //  null(object) = false(boolean)
-console.log(String(null)); // null(object) = null(string)
-console.log(String(NaN)); // NaN(Number) = NaN(string)
+// console.log(newName);  // Darshan(string) = NaN(Number)
+// console.log(newEmpty);  // ""(string) = 0(Number)
+// console.log(newNullValue); // null(object) = 0(Number)
+// console.log(newIsBoolean); // false(boolean) = false(string)
+// console.log(boolNull);  //  null(object) = false(boolean)
+// console.log(String(null)); // null(object) = null(string)
+// console.log(String(NaN)); // NaN(Number) = NaN(string)
 
-// converted Datatypes
-console.log(typeof newName);  //Number
-console.log(typeof NaN); //Number
-console.log(typeof String(null)); //String
+// // converted Datatypes
+// console.log(typeof newName);  //Number
+// console.log(typeof NaN); //Number
+// console.log(typeof String(null)); //String
+
+// ************************ Operations **********************
+
+console.log("1"+1+1); // concat starts from first string appearance before which it only performs addition
+console.log(1+1+"1"); //before string it adds then concats
+
+console.log(+true); // null + true = 0 + 1
+console.log(""+true); // 0+1
+
+let num = 100;
+console.log(num++); // first prints then increaments
+// now num = 100 
+console.log(num);
+
+let num2 = 1;
+console.log(++num2); // first increament then print
